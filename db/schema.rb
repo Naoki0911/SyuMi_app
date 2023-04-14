800a85e08304687857cdc6242f6a3f7b103396ee
naoki ban <bluefoxxexx@gmail.com>
2023-04-14 17:58:17 +0900

    お気に入り機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_11_135446) do
+ActiveRecord::Schema.define(version: 2023_04_14_024927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "sake_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sake_id"], name: "index_favorites_on_sake_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "labelings", force: :cascade do |t|
     t.bigint "sake_id", null: false
@@ -41,8 +50,36 @@ ActiveRecord::Schema.define(version: 2023_04_11_135446) do
     t.integer "rich", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_sakes_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.boolean "admin", default: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "favorites", "sakes"
+  add_foreign_key "favorites", "users"
   add_foreign_key "labelings", "labels"
   add_foreign_key "labelings", "sakes"
+  add_foreign_key "sakes", "users"
 end
